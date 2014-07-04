a34741e79ceb4c631e8bac92246daf4f57514211
TJ Usiyan <griotspeak@gmail.com>
2014-07-03 20:10:21 -0700

    Initial Commit

---
+++ Mod3/Mod3.h
@@ -0,0 +1,19 @@
+//
+//  Mod3.h
+//  Mod3
+//
+//  Created by TJ Usiyan on 7/3/14.
+//  Copyright (c) 2014 ButtonsAndLights. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mod3.
+FOUNDATION_EXPORT double Mod3VersionNumber;
+
+//! Project version string for Mod3.
+FOUNDATION_EXPORT const unsigned char Mod3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mod3/PublicHeader.h>
+
+
